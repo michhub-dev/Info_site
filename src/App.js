a76bdf56716ba42f4f60e417808f6c52688df385
Michelle David <rachaelholy@gmail.com>
2022-10-06 23:07:33 +0100

    Created MidImages component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./styles.css";
 import Header from "./components/Header";
+import MidImages from './components/MidImages';
 import Main from "./components/Main";
 import Footer from './components/Footer';
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+      <MidImages />
       <Main />
       <Footer />
     </div>
